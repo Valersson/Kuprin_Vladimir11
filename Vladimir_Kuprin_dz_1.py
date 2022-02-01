e6384dcf8c4d17b7825cb1539a68a7a2ec3ce984
Valersson <geopaks971@gmail.com>
2022-02-02 04:17:02 +0700

    Пробное что-то

--- Vladimir_Kuprin_dz_1.py
+++ Vladimir_Kuprin_dz_1.py
@@ -1,19 +1,24 @@
 <<<<<<< HEAD
 #duration
 duration = int(input())
-seconds = 0
 minutes = 0
+hours = 0
 days = 0
 for i in range (1; duration):
-    minutes += 1
-        if duration / 60 > 0:
-print(minutes)
-=======
-def convert_time(duration: int) -> str:
-    return
+    if duration / 60:
+        minutes += 1
+    elif duration / 360:
+        hours += 1
+    elif duration / 8640:
+        days += 1
+print(minutes, hours, days)
+
+#=======
+#def convert_time(duration: int) -> str:
+#    return
 
 
-duration = 400153
-result = convert_time(duration)
-print(result)
->>>>>>> origin/Vladimir_Kuprin_lesson_1
+#duration = 400153
+#result = convert_time(duration)
+#print(result)
+#>>>>>>> origin/Vladimir_Kuprin_lesson_1
